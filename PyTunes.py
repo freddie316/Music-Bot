8c4667f51496afb2458f2f3fb034dce0f5088efb
freddie316 <freddiewild@hawaii.rr.com>
2023-03-17 15:41:06 -0600

    Update PyTunes.py

--- PyTunes.py
+++ PyTunes.py
@@ -5,10 +5,36 @@ Date: Thu Mar 16 2023
 """
 
 import os
+import asyncio
 import youtube_dl
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+
+ytdl = youtube_dl.YoutubeDL()
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source: discord.AudioSource, *, data: dict, volume: float = 0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get("title")
+        self.url = data.get("url")
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(
+            None, lambda: ytdl.extract_info(url, download=not stream)
+        )
+
+        if "entries" in data:
+            # Takes the first item from a playlist
+            data = data["entries"][0]
+
+        filename = data["url"] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename), data=data)
 
 def main():
     load_dotenv()
@@ -42,19 +68,23 @@ def main():
     @bot.command()
     async def leave(ctx):
         try:
+            if ctx.voice_client.is_playing():
+                await stop(ctx)
             await ctx.voice_client.disconnect()
         except:
             await ctx.reply("I'm not connected to a voice channel.")
     
     @bot.command()
-    async def play(ctx, message):
+    async def play(ctx, url):
         try:
-            await join(ctx)
-            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio('Cancun.mp3'))
-            ctx.voice_client.play(source)
-            await ctx.reply('Playing.')
+            if ctx.voice_client is None:
+                await join(ctx)
+            async with ctx.typing():
+                source = await YTDLSource.from_url(url)
+                ctx.voice_client.play(source)
+            await ctx.reply('Now playing: {source.title}')
         except:
-            await ctx.reply("An error occured.")        
+            await ctx.reply("An error occured.")   
             
     @bot.command()
     async def stop(ctx):
