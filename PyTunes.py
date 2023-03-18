7c69a5a446e766d90f93eb9a82ff43450c240b20
freddie316 <freddiewild@hawaii.rr.com>
2023-03-17 19:07:39 -0600

    Update PyTunes.py

--- PyTunes.py
+++ PyTunes.py
@@ -8,7 +8,7 @@ import os
 import asyncio
 import yt_dlp
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 ytdl_format_options = {
@@ -24,67 +24,71 @@ ffmpeg_options = {"options": "-vn"}
 
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
 
-def main():
-    load_dotenv()
-    TOKEN = os.getenv('DISCORD_TOKEN')
-    
-    intents = discord.Intents.default()
-    intents.messages = True
-    intents.message_content = True
-    
-    bot = commands.Bot(command_prefix='!',intents=intents)
-    
-    @bot.event
-    async def on_ready():
-        print(f'{bot.user} is connected to the following guild:\n')
-        for guild in bot.guilds:
-            print(f'{guild.name} (id: {guild.id})\n')
-        
-    @bot.command()
-    async def join(ctx):
-        try:
-            channel = ctx.author.voice.channel
-        except:
-            await ctx.reply("Are you in voice channel?")  
-        if ctx.voice_client is not None:
-            return await ctx.voice_client.move_to(channel)
-        await channel.connect()
-    
-    @bot.command()
-    async def leave(ctx):
-        try:
-            if ctx.voice_client.is_playing():
-                await stop(ctx)
-            await ctx.voice_client.disconnect()
-        except:
-            await ctx.reply("I'm not connected to a voice channel.")
-    
-    @bot.command()
-    async def play(ctx, url):
-        try:
-            if ctx.voice_client is None:
-                await join(ctx)
-            async with ctx.typing():
-                source = ytdl.extract_info(url,download=True)
-                filename = ytdl.prepare_filename(source)
-                song = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(filename, **ffmpeg_options))
-                ctx.voice_client.play(song,
-                    after = lambda e: os.remove(filename)
-                )
-            await ctx.reply(f"Now playing: {source['title']}")
-        except Exception as e:
-            await ctx.reply(f"An error occured: {e}")   
-            
-    @bot.command()
-    async def stop(ctx):
-        if ctx.voice_client is None:
-            await ctx.reply("I'm not connected to a voice channel.")  
-            return
-        if not ctx.voice_client.is_playing():
-            await ctx.reply("I'm not playing anything.")  
-            return 
-        ctx.voice_client.stop()
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!',intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is connected to the following guild:\n')
+    for guild in bot.guilds:
+        print(f'{guild.name} (id: {guild.id})\n')
+    
+@bot.command()
+async def join(ctx):
+    try:
+        channel = ctx.author.voice.channel
+    except:
+        await ctx.reply("Are you in voice channel?")  
+    if ctx.voice_client is not None:
+        return await ctx.voice_client.move_to(channel)
+    await channel.connect()
+
+@bot.command()
+async def leave(ctx):
+    try:
+        if ctx.voice_client.is_playing():
+            await stop(ctx)
+        await ctx.voice_client.disconnect()
+    except:
+        await ctx.reply("I'm not connected to a voice channel.")
+
+@bot.command()
+async def play(ctx, url):
+    try:
+        if ctx.voice_client is None:
+            await join(ctx)
+        async with ctx.typing():
+            source = ytdl.extract_info(url,download=True)
+            filename = ytdl.prepare_filename(source)
+            song = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(filename, **ffmpeg_options))
+            ctx.voice_client.play(song,
+                after = lambda e: os.remove(filename)
+            )
+        await ctx.reply(f"Now playing: {source['title']}")
+    except Exception as e:
+        await ctx.reply(f"An error occured: {e}")   
+        
+@bot.command()
+async def stop(ctx):
+    if ctx.voice_client is None:
+        await ctx.reply("I'm not connected to a voice channel.")  
+        return
+    if not ctx.voice_client.is_playing():
+        await ctx.reply("I'm not playing anything.")  
+        return 
+    ctx.voice_client.stop()
+    
+@bot.command()
+async def ping(ctx):
+    await ctx.reply("Pong!") 
+
+def main():
     bot.run(TOKEN)
     return
 
