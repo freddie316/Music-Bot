058d019cff70a6fa95e26cd5716ecc3a885aa2c8
freddie316 <freddiewild@hawaii.rr.com>
2023-03-19 19:29:20 -0600

    Converted to Cog structure
    Moved music related commands into a cog class

--- PyTunes.py
+++ PyTunes.py
@@ -33,106 +33,107 @@ intents.messages = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!',intents=intents)
-repeatFlag = False
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is connected to the following guild:\n')
     for guild in bot.guilds:
         print(f'{guild.name} (id: {guild.id})\n')
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.repeatFlag = False
     
-@bot.command()
-async def join(ctx):
-    try:
-        channel = ctx.author.voice.channel
-    except:
-        await ctx.reply("Are you in voice channel?")
-        return
-    if ctx.voice_client is not None:
-        return await ctx.voice_client.move_to(channel)
-    await channel.connect()
-    afk_timer.start()
+    @commands.command()
+    async def join(self, ctx):
+        try:
+            channel = ctx.author.voice.channel
+        except:
+            await ctx.reply("Are you in voice channel?")
+            return
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+        await channel.connect()
+        self.afk_timer.start()
 
-@bot.command()
-async def leave(ctx):
-    try:
-        if ctx.voice_client.is_playing():
-            await stop(ctx)
-        await ctx.voice_client.disconnect()
-    except:
-        await ctx.reply("I'm not connected to a voice channel.")
-        return
-    afk_timer.stop()
+    @commands.command()
+    async def leave(self, ctx):
+        try:
+            if ctx.voice_client.is_playing():
+                await self.stop(ctx)
+            await ctx.voice_client.disconnect()
+        except:
+            await ctx.reply("I'm not connected to a voice channel.")
+            return
+        self.afk_timer.stop()
 
-@bot.command()
-async def play(ctx, url):
-    global repeatFlag
-    try:
-        if ctx.voice_client is None:
-            await join(ctx)
-        if ctx.voice_client.is_playing():
-            if repeatFlag:
-                repeatFlag = False
-                ctx.voice_client.stop()
-                repeatFlag = True
-            else:
-                ctx.voice_client.stop()
-        async with ctx.typing():
-            source = ytdl.extract_info(url,download=True)
-            filename = ytdl.prepare_filename(source)
+    @commands.command()
+    async def play(self, ctx, url):
+        try:
+            if ctx.voice_client is None:
+                await self.join(ctx)
+            if ctx.voice_client.is_playing():
+                if self.repeatFlag:
+                    self.repeatFlag = False
+                    ctx.voice_client.stop() # this might not be working
+                    self.repeatFlag = True
+                else:
+                    ctx.voice_client.stop()
+            async with ctx.typing():
+                source = ytdl.extract_info(url,download=True)
+                filename = ytdl.prepare_filename(source)
+                song = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(filename, **ffmpeg_options))
+                ctx.voice_client.play(song,
+                    after = lambda e: self.cleanup(ctx,filename)
+                )
+            await ctx.reply(f"Now playing: {source['title']}")
+        except Exception as e:
+            await ctx.reply(f"An error occured: {e}")   
+
+    def cleanup(self, ctx, filename):
+        if self.repeatFlag:
             song = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(filename, **ffmpeg_options))
             ctx.voice_client.play(song,
-                after = lambda e: cleanup(ctx,filename)
+                after = lambda e: self.cleanup(ctx,filename)
             )
-        await ctx.reply(f"Now playing: {source['title']}")
-    except Exception as e:
-        await ctx.reply(f"An error occured: {e}")   
-
-def cleanup(ctx,filename):
-    global repeatFlag
-    if repeatFlag:
-        song = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(filename, **ffmpeg_options))
-        ctx.voice_client.play(song,
-            after = lambda e: cleanup(ctx,filename)
-        )
-    else:
-        os.remove(filename)
-        afk_timer.restart()
+        else:
+            os.remove(filename)
+            self.afk_timer.restart()
+            
+    @commands.command()
+    async def repeat(self,ctx):
+        if self.repeatFlag:
+            self.repeatFlag = False
+        else:
+            self.repeatFlag = True
+        await ctx.reply(f"Repeat mode: {self.repeatFlag}")
         
- 
-@bot.command()
-async def repeat(ctx):
-    global repeatFlag
-    if repeatFlag:
-        repeatFlag = False
-    else:
-        repeatFlag = True
-    await ctx.reply(f"Repeat mode: {repeatFlag}")
-     
- 
-@bot.command()
-async def stop(ctx):
-    if ctx.voice_client is None:
-        await ctx.reply("I'm not connected to a voice channel.")  
+    
+    @commands.command()
+    async def stop(self, ctx):
+        if ctx.voice_client is None:
+            await ctx.reply("I'm not connected to a voice channel.")  
+            return
+        if not ctx.voice_client.is_playing():
+            await ctx.reply("I'm not playing anything.")  
+            return 
+        ctx.voice_client.stop()
+        
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.reply("Pong!") 
+        
+    @tasks.loop(seconds = 0)
+    async def afk_timer(self):
+        await asyncio.sleep(300) # 5 minutes
+        if not bot.voice_clients[0].is_playing():
+            await bot.voice_clients[0].disconnect()
+            self.afk_timer.stop()
         return
-    if not ctx.voice_client.is_playing():
-        await ctx.reply("I'm not playing anything.")  
-        return 
-    ctx.voice_client.stop()
-    
-@bot.command()
-async def ping(ctx):
-    await ctx.reply("Pong!") 
-    
-@tasks.loop(seconds = 0)
-async def afk_timer():
-    await asyncio.sleep(300) # 5 minutes
-    if not bot.voice_clients[0].is_playing():
-        await bot.voice_clients[0].disconnect()
-        afk_timer.stop()
-    return
 
 def main():
+    bot.add_cog(Music(bot))
     bot.run(TOKEN)
     return
 
